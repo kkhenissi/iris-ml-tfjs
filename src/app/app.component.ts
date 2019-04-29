47912385d786c93f6a2ca7be546d4ec2586c89c0
kkhenissi <kkhenissi@yahoo.fr>
2019-04-30 00:13:00 +0200

    add tensors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,9 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 export class AppComponent implements OnInit  {
 
   public tensors: tf.Tensor2D[] = [];
+  public model: tf.Sequential;
+  public learningRate: number = 0.001;
+  public modelCreated: boolean;
 
   ngOnInit(): void {
     this.tensors[ 'X'] = tf.tensor2d([45, 11, 25, 31, 45, 26, 45, 21, 22, 15, 44, 66], [3, 4]);
@@ -22,4 +25,39 @@ export class AppComponent implements OnInit  {
   onMult() {
     this.tensors['Z'] = this.tensors['X'].matMul(this.tensors['Y']);
   }
-}
+
+  onTranspose() {
+     this.tensors['Z']=this.tensors['X'].transpose();
+  }
+  onSignoide() {
+    this.tensors['Z']=tf.sigmoid(this.tensors['X']);
+  }
+  onRelu() {
+    this.tensors['Z']=tf.relu(this.tensors['X']);
+
+  }
+  onCreateModel() {
+  
+    this.model = tf.sequential();
+
+    this.model.add(tf.layers.dense({
+      units:10,
+      activation: 'sigmoid',
+      inputShape: [4]
+    }));
+
+    this.model.add(tf.layers.dense({
+      units:3,
+      activation: 'softmax'
+    }));
+
+    this.model.compile({
+      optimizer:tf.train.adam(this.learningRate),
+      loss: tf.losses.meanSquaredError,
+      metrics: ['accuracy']
+
+    });
+  
+    this.modelCreated = true;
+  }
+ }
